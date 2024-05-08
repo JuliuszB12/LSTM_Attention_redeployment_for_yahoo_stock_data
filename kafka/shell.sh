80193202826882b46f1695143170a676cee79a9c
Juliusz Juzaszek <jjuzaszek@gmail.com>
2024-05-08 03:36:24 +0200

    Adding Zookeeper, Kafka, MLflow setup.sh

---
+++ kafka/shell.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Updating packages..."
+sudo apt-get update -y
+
+echo "Installing required packages..."
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+
+echo "Configuring Docker repository..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+echo "Installing Docker..."
+sudo apt-get install docker-ce docker-ce-cli -y
+
+echo "Adding user to Docker group..."
+sudo usermod -a -G docker $USER
+
+echo "Installing Docker Compose..."
+sudo apt-get install docker-compose-plugin -y
+
+echo "Starting Zookeeper and Kafka..."
+docker compose up
+
+echo "Setup complete!"
