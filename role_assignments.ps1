e601f9e225a7473fd00bda314d45aae41b535400
JuliuszB12 <68758875+JuliuszB12@users.noreply.github.com>
2024-05-09 23:08:12 +0200

    Add file role_assignments.ps1

---
+++ role_assignments.ps1
@@ -0,0 +1,20 @@
+$subscriptionId = "38ca6696-5c82-4571-b2af-bf3f256cf663"
+$resourceGroupName = "rocket_test_1"
+$vmName = "airflow"
+$storageAccountName = "kafkastockdata1"
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+$storageAccountResourceId = $storageAccount.Id
+New-AzRoleAssignment -ObjectId $vm.Identity.PrincipalId -RoleDefinitionName "Contributor" -Scope $storageAccountResourceId
+$vmName = "airflow"
+$storageAccountName = "kafkastockdata1"
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+$storageAccountResourceId = $storageAccount.Id
+New-AzRoleAssignment -ObjectId $vm.Identity.PrincipalId -RoleDefinitionName "Storage Blob Data Contributor" -Scope $storageAccountResourceId
+$vmName = "airflow"
+$amlWorkspaceName = "mlserving"
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+$amlWorkspace = Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType "Microsoft.MachineLearningServices/workspaces" -Name $amlWorkspaceName
+$amlWorkspaceResourceId = $amlWorkspace.ResourceId
+ New-AzRoleAssignment -ObjectId $vm.Identity.PrincipalId -RoleDefinitionName "Contributor" -Scope $amlWorkspaceResourceId
