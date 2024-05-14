1120d14e93ab73388f18b121e827ecf41a5bfc90
JuliuszB12 <68758875+JuliuszB12@users.noreply.github.com>
2024-05-14 19:26:06 +0200

    Update __init__.py

--- FunctionAPI/Function/__init__.py
+++ FunctionAPI/Function/__init__.py
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     msi_auth = MsiAuthentication()
     ws = Workspace(subscription_id="38ca6696-5c82-4571-b2af-bf3f256cf663", 
-                   resource_group="rocket_test0987501", 
+                   resource_group="rocket_test01234", 
                    workspace_name="mlserving", 
                    auth=msi_auth)
     service = AksWebservice(ws, 'lstm-service')
